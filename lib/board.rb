e2823aa1b949a1999dd79d23278bd6d8a195ba1f
icebergHugger <kolbeinn.floki@gmail.com>
2021-01-04 02:52:25 +0000

    Made create_board function

--- lib/board.rb
+++ lib/board.rb
@@ -2,9 +2,28 @@
 class Cell
   attr_accessor :value, :piece
 
-  def Initialize(value, piece)
+  def initialize(value, piece=nil)
     @value = value
     @piece = piece
   end
 
 end
+
+def create_board
+
+  8.times do |col|
+    8.times do |row|
+      new_cell = Cell.new([col + 1, row + 1])
+    end
+  end
+
+end
+
+#def Find_cells
+
+
+#def fill_cell
+
+
+
+#def set_up
